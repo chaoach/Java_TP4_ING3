51718432f1371a9f8671e41f26abc3dd9ee64d2d
PC <PC@LAPTOP-QUB8AJV2>
2021-11-29 14:09:28 +0100

    First Commit

---
+++ src/main/java/vue/MaFenetre.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vue;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author PC
+ */
+public class MaFenetre extends JFrame {
+    //Constructeur par défaut
+    public MaFenetre(){ //Constructeur
+        setSize (300, 150); //donne la taille de la fenetre
+        setTitle("Ma premiere fenetre"); //Donne un titre a la fenetre
+    }
+}
